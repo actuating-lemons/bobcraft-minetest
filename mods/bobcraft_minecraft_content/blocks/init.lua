1ed75604a80fd1c11709164fcfaf1c128a9410d3
ACBob <acbobcat@protonmail.com>
2021-02-16 03:41:59 +0000

    further refactoring of directory structure

--- mods/bobcraft_minecraft_content/blocks/init.lua
+++ mods/bobcraft_minecraft_content/blocks/init.lua
@@ -238,4 +238,3 @@ minetest.register_node("bobcraft_blocks:cobblestone", {
 local modpath = minetest.get_modpath("bobcraft_blocks")
 dofile(modpath .. "/interactions.lua")
 dofile(modpath .. "/aliases.lua")
-dofile(modpath .. "/dropitem.lua")
