c027aab32396de5489b7433a94165e2ae470fbe7
ACBob <acbobcat@protonmail.com>
2021-02-16 01:28:54 +0000

    care about fitting into inventory, drop single item

--- mods/nodes/dropitem.lua
+++ mods/nodes/dropitem.lua
@@ -7,13 +7,23 @@ local item_scale = 0.25
 local item_grab_radius = 0.1 -- one tenth block?
 local item_flyto_radius = 2 -- two blocks?
 local break_flyto_time = 20 -- In MC 1.2.5 it's 10 update calls when a block breaks, we do 20
+local drop_flyto_time = 80 -- In MC 1.2.5, it's 40 update calls. We do 80.
 
-local disable_physics = function(object, luanetity)
-	if luanetity.physical_state then
-		luanetity.physical_state = false
+local disable_physics = function(object, luaentity)
+	if luaentity.physical_state then
+		luaentity.physical_state = false
 		object:set_properties({physical = false})
 		object:set_velocity({x=0, y=0, z=0})
 		object:set_acceleration({x=0, y=0, z=0})
+	end
+end
+
+local enable_physics = function(object, luaentity)
+	if not luaentity.physical_state then
+		luaentity.physical_state = true
+		object:set_properties({physical = true})
+		object:set_velocity({x=0, y=0, z=0})
+		object:set_acceleration({x=0, y=-gravity, z=0})
 	end
 end
 
@@ -64,12 +74,7 @@ local item_entity = { -- reference https://rubenwardy.com/minetest_modding_book/
 	end,
 
 	enable_physics = function(self)
-		if not self.physical_state then
-			self.physical_state = true
-			self.object:set_properties({physical = true})
-			self.object:set_velocity({x=0, y=0, z=0})
-			self.object:set_acceleration({x=0, y=-gravity, z=0})
-		end
+		enable_physics(self.object, self)
 	end,
 
 	disable_physics = function(self)
@@ -103,18 +108,20 @@ minetest.register_globalstep(function(dtime)
 
 		for j, object in ipairs(minetest.get_objects_inside_radius(pos, 16)) do
 			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-				if object:get_luaentity().magnetting and vector.distance(pos, object:get_pos()) <= item_flyto_radius then
-					disable_physics(object, object:get_luaentity())
+				if object:get_luaentity().magnetting and vector.distance(pos, object:get_pos()) <= item_flyto_radius and
+					inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then -- we pretend to not be in the radius if the player doesn't have space
+				disable_physics(object, object:get_luaentity())
 
-					local old_pos = object:get_pos()
-					local vec = vector.subtract(pos, old_pos)
-					vec = vector.add(old_pos, vector.divide(vec, 2))
-					object:move_to(vec)
+				local old_pos = object:get_pos()
+				local vec = vector.subtract(pos, old_pos)
+				vec = vector.add(old_pos, vector.divide(vec, 2))
+				object:move_to(vec)
 
-					-- we also use magnetting as a rite of passage to picking up
-					if vector.distance(pos, object:get_pos()) <= item_grab_radius then
-						if object:get_luaentity().itemstring then
-							inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
+				-- we also use magnetting as a rite of passage to picking up
+				if vector.distance(pos, object:get_pos()) <= item_grab_radius then
+					if object:get_luaentity().itemstring then
+						local left_itemstack = inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
+						if left_itemstack and left_itemstack:get_count() == 0 then
 							minetest.sound_play("item_pickup", {
 								pos = pos,
 								max_hear_distance = 5,
@@ -124,6 +131,10 @@ minetest.register_globalstep(function(dtime)
 							object:remove()
 						end
 					end
+				end
+				elseif object:get_luaentity().magnetting then
+					-- we're magnetting, but  we're not in the magnet radius
+					enable_physics(object, object:get_luaentity())
 				elseif object:get_luaentity().magnettime <= 0 then
 					object:get_luaentity().magnetting = true
 				else
@@ -149,5 +160,24 @@ function minetest.handle_node_drops(pos, drops, digger)
 				dropped_item:get_luaentity().magnettime = break_flyto_time
 			end
 		end 
+	end
+end
+
+-- for player dropping
+function minetest.item_drop(itemstack, dropper, pos)
+	if dropper and dropper:is_player() then
+		local dir = dropper:get_look_dir()
+		local position = {x=pos.x, y=pos.y+1.2, z=pos.z}
+		local count = 1 -- TODO: stack dropping
+		local item = itemstack:take_item(count)
+		local obj = minetest.add_item(position, item)
+		if obj then
+			dir.x = dir.x*4
+			dir.y = dir.y*4 + 2
+			dir.z = dir.z*4
+			obj:set_velocity(dir)
+			obj:get_luaentity().magnettime = drop_flyto_time
+			return itemstack
+		end
 	end
 end
