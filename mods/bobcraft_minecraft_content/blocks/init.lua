404df0e63e584611a5a65c7da69ffd78922b2230
ACBob <acbobcat@protonmail.com>
2021-02-17 18:38:00 +0000

    killed all current content, got hardness working(?)

--- mods/bobcraft_minecraft_content/blocks/init.lua
+++ mods/bobcraft_minecraft_content/blocks/init.lua
@@ -1,240 +1,267 @@
--- Overworld blocks
+local function register_block(nodename, nodedef)
+	nodedef = tool_values.setup_group_values(nodename, nodedef)
+	minetest.log(dump(nodedef))
+	minetest.register_node(nodename, nodedef)
+end
 
-minetest.register_node("bobcraft_blocks:grass_block", {
-	description = "Grass Block",
-	tiles = {"grass_top.png",
-	"dirt.png",
-	"grass_side.png"},
-	is_ground_content = true,
-	groups = {soil = 1, crumbly = 1},
-	sounds = bobcraft_sounds.node_sound_planty()
+minetest.register_node("bobcraft_blocks:bedrock", {
+		description = "Bedrock",
+		tiles = {"bedrock.png"},
+		groups = {},
+		sounds = bobcraft_sounds.node_sound_stone()
 })
-
-minetest.register_node("bobcraft_blocks:dirt", {
-	description = "Dirt Block",
-	tiles = {"dirt.png"},
-	is_ground_content = true,
-	groups = {soil = 1, crumbly = 2},
-	sounds = bobcraft_sounds.node_sound_earthy()
-})
-
-minetest.register_node("bobcraft_blocks:stone", {
+register_block("bobcraft_blocks:stone", {
 	description = "Stone",
 	tiles = {"stone.png"},
+	diggable = true,
 	is_ground_content = true,
-	groups = {cracky=1},
 	sounds = bobcraft_sounds.node_sound_stone(),
-	drop = "bobcraft_blocks:cobblestone"
+	
+	hardness = 1,
+	groups = {pickaxe=1}
 })
 
-minetest.register_node("bobcraft_blocks:rose", {
-	description = "Singular Rose",
-	tiles = {"rose.png"},
-	wield_image = "rose.png",
-	is_ground_content = true,
-	groups = {oddly_breakable_by_hand=3},
-	sounds = bobcraft_sounds.node_sound_planty(),
-	drawtype = "plantlike",
-	walkable = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
-	},
-	paramtype = "light",
-	sunlight_propagates = true
-})
 
-minetest.register_node("bobcraft_blocks:cactus", {
-	description = "Cactus",
-	tiles = {"cactus_side.png"}, -- TODO: cactus model
-	groups = {oddly_breakable_by_hand=3},
-	sounds = bobcraft_sounds.node_sound_planty()
-})
+-- -- Overworld blocks
 
-minetest.register_node("bobcraft_blocks:sand", {
-	description = "Sand",
-	tiles = {"sand.png"},
-	groups = {crumbly = 3, falling_node = 1, sand = 1},
-	sounds = bobcraft_sounds.node_sound_sand(),
-})
-minetest.register_node("bobcraft_blocks:gravel", {
-	description = "Gravel",
-	tiles = {"gravel.png"},
-	groups = {crumbly = 3, falling_node = 1},
-	sounds = bobcraft_sounds.node_sound_gravel(),
-})
-minetest.register_node("bobcraft_blocks:sandstone", {
-	description = "Sand",
-	tiles = {"sandstone_top.png", "sandstone_bottom.png", "sandstone_side.png"},
-	groups = {crumbly = 3, sand = 1},
-	sounds = bobcraft_sounds.node_sound_sandstone(),
-})
+-- minetest.register_node("bobcraft_blocks:grass_block", {
+-- 	description = "Grass Block",
+-- 	tiles = {"grass_top.png",
+-- 	"dirt.png",
+-- 	"grass_side.png"},
+-- 	is_ground_content = true,
+-- 	groups = {hand=1, shovel=1},
+-- 	sounds = bobcraft_sounds.node_sound_planty()
+-- })
 
-minetest.register_node("bobcraft_blocks:snow_layer", {
-	description = "Snow",
-	tiles = {"snow.png"},
-	groups = {oddly_breakable_by_hand=2},
-	sounds = bobcraft_sounds.node_sound_snow(),
-	paramtype = "light",
-	buildable_to = true,
-	floodable = true,
-	drawtype = "nodebox",
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
-		},
-	},
-	collision_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, -8 / 16, 0.5},
-		},
-	},
-})
-minetest.register_node("bobcraft_blocks:snowy_grass_block", {
-	description = "Snow Covered Grass Block",
-	tiles = {"grass_top_snow.png",
-	"dirt.png",
-	"grass_side_snow.png"},
-	is_ground_content = true,
-	groups = {soil = 1, crumbly = 1},
-	sounds = bobcraft_sounds.node_sound_planty(),
+-- minetest.register_node("bobcraft_blocks:dirt", {
+-- 	description = "Dirt Block",
+-- 	tiles = {"dirt.png"},
+-- 	is_ground_content = true,
+-- 	groups = {hand=1, shovel=1},
+-- 	hardness = 0.5,
+-- 	sounds = bobcraft_sounds.node_sound_earthy()
+-- })
 
-	on_construct = function(pos)
-		pos.y = pos.y - 1
-		if minetest.get_node(pos).name == "bobcraft_blocks:grass_block" then
-			minetest.set_node(pos, {name="bobcraft_blocks:snowy_grass_block"})
-		end
-	end,
-})
+-- minetest.register_node("bobcraft_blocks:stone", {
+-- 	description = "Stone",
+-- 	tiles = {"stone.png"},
+-- 	is_ground_content = true,
+-- 	groups = {tool_pickaxe=1},
+-- 	hardness = 1,
+-- 	sounds = bobcraft_sounds.node_sound_stone(),
+-- 	drop = "bobcraft_blocks:cobblestone"
+-- })
 
--- liquids
-minetest.register_node("bobcraft_blocks:water_source",{
-	description = "Water",
-	drawtype = "liquid",
-	waving = 3,
-	tiles= {"water_still.png"},
-	special_tiles = {
-		{
-			name = "water_still.png",
-			backface_culling = false,
-		},
-		{
-			name = "water_still.png",
-			backface_culling = true,
-		},
-	},
-	use_texture_alpha = "blend",
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = false,
-	is_ground_content = false,
-	drop = "",
-	drowning = 1,
-	liquidtype = "source",
-	liquid_alternative_flowing = "bobcraft_blocks:water_flowing",
-	liquid_alternative_source = "bobcraft_blocks:water_source",
-	liquid_viscosity = 1,
-	sounds = bobcraft_sounds.node_sound_water(),
-	groups = {not_in_creative_inventory=1}
-})
-minetest.register_node("bobcraft_blocks:water_flowing",{
-	description = "Water",
-	drawtype = "flowingliquid",
-	waving = 3,
-	tiles= {"water_still.png"},
-	special_tiles = {
-		{
-			name = "water_flow.png",
-			backface_culling = false,
-		},
-		{
-			name = "water_flow.png",
-			backface_culling = true,
-		},
-	},
-	use_texture_alpha = "blend",
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	is_ground_content = false,
-	drop = "",
-	drowning = 1,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "bobcraft_blocks:water_flowing",
-	liquid_alternative_source = "bobcraft_blocks:water_source",
-	liquid_viscosity = 1,
-	sounds = bobcraft_sounds.node_sound_water(),
-	groups = {not_in_creative_inventory=1}
-})
+-- minetest.register_node("bobcraft_blocks:rose", {
+-- 	description = "Singular Rose",
+-- 	tiles = {"rose.png"},
+-- 	wield_image = "rose.png",
+-- 	is_ground_content = true,
+-- 	groups = {oddly_breakable_by_hand=3},
+-- 	sounds = bobcraft_sounds.node_sound_planty(),
+-- 	drawtype = "plantlike",
+-- 	walkable = false,
+-- 	selection_box = {
+-- 		type = "fixed",
+-- 		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
+-- 	},
+-- 	paramtype = "light",
+-- 	sunlight_propagates = true
+-- })
 
--- the token unbreakable block
--- we have some code here to make it breakable in CREATIVE only
-local bedrock_groups
-if minetest.settings:get_bool("creative_mode") then
-	bedrock_groups = {oddly_breakable_by_hand=1}
-else
-	bedrock_groups = {}
-end
-minetest.register_node("bobcraft_blocks:bedrock", {
-	description = "Bedrock",
-	tiles = {"bedrock.png"},
-	is_ground_content = false, -- We *are* technically, but we also shouldn't be over-ridden
-	groups = bedrock_groups,
-	sounds = bobcraft_sounds.node_sound_stone()
-})
+-- minetest.register_node("bobcraft_blocks:cactus", {
+-- 	description = "Cactus",
+-- 	tiles = {"cactus_side.png"}, -- TODO: cactus model
+-- 	groups = {oddly_breakable_by_hand=3},
+-- 	sounds = bobcraft_sounds.node_sound_planty()
+-- })
 
--- Other blocks
+-- minetest.register_node("bobcraft_blocks:sand", {
+-- 	description = "Sand",
+-- 	tiles = {"sand.png"},
+-- 	groups = {crumbly = 3, falling_node = 1, sand = 1},
+-- 	sounds = bobcraft_sounds.node_sound_sand(),
+-- })
+-- minetest.register_node("bobcraft_blocks:gravel", {
+-- 	description = "Gravel",
+-- 	tiles = {"gravel.png"},
+-- 	groups = {crumbly = 3, falling_node = 1},
+-- 	sounds = bobcraft_sounds.node_sound_gravel(),
+-- })
+-- minetest.register_node("bobcraft_blocks:sandstone", {
+-- 	description = "Sand",
+-- 	tiles = {"sandstone_top.png", "sandstone_bottom.png", "sandstone_side.png"},
+-- 	groups = {crumbly = 3, sand = 1},
+-- 	sounds = bobcraft_sounds.node_sound_sandstone(),
+-- })
 
-minetest.register_node("bobcraft_blocks:planks", {
-	description = "Planks",
-	tiles = {"planks.png"},
-	is_ground_content = false,
-	groups = {choppy=2},
-	sounds = bobcraft_sounds.node_sound_wood()
-})
+-- minetest.register_node("bobcraft_blocks:snow_layer", {
+-- 	description = "Snow",
+-- 	tiles = {"snow.png"},
+-- 	groups = {oddly_breakable_by_hand=2},
+-- 	sounds = bobcraft_sounds.node_sound_snow(),
+-- 	paramtype = "light",
+-- 	buildable_to = true,
+-- 	floodable = true,
+-- 	drawtype = "nodebox",
+-- 	node_box = {
+-- 		type = "fixed",
+-- 		fixed = {
+-- 			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
+-- 		},
+-- 	},
+-- 	collision_box = {
+-- 		type = "fixed",
+-- 		fixed = {
+-- 			{-0.5, -0.5, -0.5, 0.5, -8 / 16, 0.5},
+-- 		},
+-- 	},
+-- })
+-- minetest.register_node("bobcraft_blocks:snowy_grass_block", {
+-- 	description = "Snow Covered Grass Block",
+-- 	tiles = {"grass_top_snow.png",
+-- 	"dirt.png",
+-- 	"grass_side_snow.png"},
+-- 	is_ground_content = true,
+-- 	groups = {soil = 1, crumbly = 1},
+-- 	sounds = bobcraft_sounds.node_sound_planty(),
 
-minetest.register_node("bobcraft_blocks:glass", {
-	description = "Glass Block",
-	tiles = {"glass.png"},
-	is_ground_content = false,
-	groups = {cracky = 3, oddly_breakable_by_hand = 3},
-	sounds = bobcraft_sounds.node_sound_glass(),
-	drawtype = "glasslike",
-	paramtype = "light",
-	drop = {}, -- drop nothing
-	sunlight_propagates = true
-})
+-- 	on_construct = function(pos)
+-- 		pos.y = pos.y - 1
+-- 		if minetest.get_node(pos).name == "bobcraft_blocks:grass_block" then
+-- 			minetest.set_node(pos, {name="bobcraft_blocks:snowy_grass_block"})
+-- 		end
+-- 	end,
+-- })
 
-minetest.register_node("bobcraft_blocks:log", {
-	description = "Log",
-	tiles = {"log_top.png", "log_top.png", "log_side.png"},
-	is_ground_content = false,
-	groups = {choppy=1},
-	sounds = bobcraft_sounds.node_sound_wood()
-})
-minetest.register_node("bobcraft_blocks:leaves", {
-	description = "Leaves",
-	drawtype = "allfaces_optional",
-	tiles = {"leaves.png"},
-	is_ground_content = false,
-	groups = {oddly_breakable_by_hand=1},
-	sounds = bobcraft_sounds.node_sound_planty()
-})
+-- -- liquids
+-- minetest.register_node("bobcraft_blocks:water_source",{
+-- 	description = "Water",
+-- 	drawtype = "liquid",
+-- 	waving = 3,
+-- 	tiles= {"water_still.png"},
+-- 	special_tiles = {
+-- 		{
+-- 			name = "water_still.png",
+-- 			backface_culling = false,
+-- 		},
+-- 		{
+-- 			name = "water_still.png",
+-- 			backface_culling = true,
+-- 		},
+-- 	},
+-- 	use_texture_alpha = "blend",
+-- 	paramtype = "light",
+-- 	walkable = false,
+-- 	pointable = false,
+-- 	diggable = false,
+-- 	buildable_to = false,
+-- 	is_ground_content = false,
+-- 	drop = "",
+-- 	drowning = 1,
+-- 	liquidtype = "source",
+-- 	liquid_alternative_flowing = "bobcraft_blocks:water_flowing",
+-- 	liquid_alternative_source = "bobcraft_blocks:water_source",
+-- 	liquid_viscosity = 1,
+-- 	sounds = bobcraft_sounds.node_sound_water(),
+-- 	groups = {not_in_creative_inventory=1}
+-- })
+-- minetest.register_node("bobcraft_blocks:water_flowing",{
+-- 	description = "Water",
+-- 	drawtype = "flowingliquid",
+-- 	waving = 3,
+-- 	tiles= {"water_still.png"},
+-- 	special_tiles = {
+-- 		{
+-- 			name = "water_flow.png",
+-- 			backface_culling = false,
+-- 		},
+-- 		{
+-- 			name = "water_flow.png",
+-- 			backface_culling = true,
+-- 		},
+-- 	},
+-- 	use_texture_alpha = "blend",
+-- 	paramtype = "light",
+-- 	walkable = false,
+-- 	pointable = false,
+-- 	diggable = false,
+-- 	buildable_to = true,
+-- 	is_ground_content = false,
+-- 	drop = "",
+-- 	drowning = 1,
+-- 	liquidtype = "flowing",
+-- 	liquid_alternative_flowing = "bobcraft_blocks:water_flowing",
+-- 	liquid_alternative_source = "bobcraft_blocks:water_source",
+-- 	liquid_viscosity = 1,
+-- 	sounds = bobcraft_sounds.node_sound_water(),
+-- 	groups = {not_in_creative_inventory=1}
+-- })
 
-minetest.register_node("bobcraft_blocks:cobblestone", {
-	description = "Cobblestone",
-	tiles = {"cobblestone.png"},
-	is_ground_content = true,
-	groups = {cracky=1},
-	sounds = bobcraft_sounds.node_sound_stone()
-})
+-- -- the token unbreakable block
+-- -- we have some code here to make it breakable in CREATIVE only
+-- local bedrock_groups
+-- if minetest.settings:get_bool("creative_mode") then
+-- 	bedrock_groups = {oddly_breakable_by_hand=1}
+-- else
+-- 	bedrock_groups = {}
+-- end
+-- minetest.register_node("bobcraft_blocks:bedrock", {
+-- 	description = "Bedrock",
+-- 	tiles = {"bedrock.png"},
+-- 	is_ground_content = false, -- We *are* technically, but we also shouldn't be over-ridden
+-- 	groups = bedrock_groups,
+-- 	sounds = bobcraft_sounds.node_sound_stone()
+-- })
 
-local modpath = minetest.get_modpath("bobcraft_blocks")
-dofile(modpath .. "/interactions.lua")
-dofile(modpath .. "/aliases.lua")
+-- -- Other blocks
+
+-- minetest.register_node("bobcraft_blocks:planks", {
+-- 	description = "Planks",
+-- 	tiles = {"planks.png"},
+-- 	is_ground_content = false,
+-- 	groups = {tool_axe=1},
+-- 	hardness = 2,
+-- 	sounds = bobcraft_sounds.node_sound_wood()
+-- })
+
+-- minetest.register_node("bobcraft_blocks:glass", {
+-- 	description = "Glass Block",
+-- 	tiles = {"glass.png"},
+-- 	is_ground_content = false,
+-- 	groups = {cracky = 3, oddly_breakable_by_hand = 3},
+-- 	sounds = bobcraft_sounds.node_sound_glass(),
+-- 	drawtype = "glasslike",
+-- 	paramtype = "light",
+-- 	drop = {}, -- drop nothing
+-- 	sunlight_propagates = true
+-- })
+
+-- minetest.register_node("bobcraft_blocks:log", {
+-- 	description = "Log",
+-- 	tiles = {"log_top.png", "log_top.png", "log_side.png"},
+-- 	is_ground_content = false,
+-- 	groups = {tool_axe=1},
+-- 	sounds = bobcraft_sounds.node_sound_wood()
+-- })
+-- minetest.register_node("bobcraft_blocks:leaves", {
+-- 	description = "Leaves",
+-- 	drawtype = "allfaces_optional",
+-- 	tiles = {"leaves.png"},
+-- 	is_ground_content = false,
+-- 	groups = {oddly_breakable_by_hand=1},
+-- 	sounds = bobcraft_sounds.node_sound_planty()
+-- })
+
+-- minetest.register_node("bobcraft_blocks:cobblestone", {
+-- 	description = "Cobblestone",
+-- 	tiles = {"cobblestone.png"},
+-- 	is_ground_content = true,
+-- 	groups = {cracky=1},
+-- 	sounds = bobcraft_sounds.node_sound_stone()
+-- })
+
+-- local modpath = minetest.get_modpath("bobcraft_blocks")
+-- dofile(modpath .. "/interactions.lua")
+-- dofile(modpath .. "/aliases.lua")
--- mods/bobcraft_outside_deps/tool_globals/init.lua
+++ mods/bobcraft_outside_deps/tool_globals/init.lua
@@ -62,8 +62,8 @@ for i=1, #materials do
 	end
 end
 
-local setup_values = function()
-	local function calculate_tool(newgroups, hardness, material, tool, actual_rating, expected_rating)
+tool_values.setup_group_values = function(nodename, nodedef)
+	local function calculate_tool(newgroups, hardness, material, tool, actual_rating, expected_rating, toolstring)
 		-- Minecraft 1.2.5 has validity based on a list of blocks.
 		-- Sad!
 		-- I'm using MineClone 2's solution here, which seems to be basing it on the actual_rating of the tool vs. the expected_rating.
@@ -72,45 +72,39 @@ local setup_values = function()
 		local time = 1
 
 		if actual_rating >= expected_rating then
-			validity_factor = 1
+			validity_factor = 1.5
 		else
 			validity_factor = 5
 		end
 
 		local speed_multiplier = tool_values.correct_material_efficiency[material]
+		time = (hardness * validity_factor) / speed_multiplier
 		if time <= 0.05 then
 			time = 0
 		else
 			time = math.ceil(time * 20) / 20
 		end
 
-		table.insert(tool_values.times[tool .. "_" .. material], time)
-		newgroups[tool .. "_" .. material] = #tool_values.times[tool .. "_" .. material]
+		table.insert(tool_values.times[toolstring], time)
+		newgroups[toolstring] = #tool_values.times[toolstring]
 		return newgroups
 	end
+	local changed = false
+	local newgroups = table.copy(nodedef.groups)
+	local hardness = nodedef.hardness or 0
+	for i, tool in pairs(tool_types) do
+		if nodedef.groups[tool] then
+			for j=1, #materials do
+				local toolstring = tool .. "_" .. materials[j]
 
-	for nodename, nodedef in pairs(minetest.registered_nodes) do
-		local changed = false
-		local newgroups = table.copy(nodedef.groups)
-		local hardness = nodedef.hardness or 0
-		for i, tool in pairs(tool_types) do
-			if nodedef.groups[tool] then
-				for j=1, #materials do
-					newgroups = calculate_tool(newgroups, hardness, materials[j], tool, j, nodedef.groups[tool])
-					changed = true
-				end
+				newgroups = calculate_tool(newgroups, hardness, materials[j], tool, j, nodedef.groups[tool], toolstring)
+				changed = true
 			end
 		end
-
-		if changed then
-			minetest.override_item(
-				nodename, {
-					groups = newgroups
-				}
-			)
-		end
 	end
-end
 
--- Call as the last step after mods load
-minetest.register_on_mods_loaded(setup_values)
+	if changed then
+		nodedef.groups = newgroups
+	end
+	return nodedef
+end
