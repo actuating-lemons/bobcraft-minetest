22cb1063a036366deed537afe4002e28e71309eb
ACBob <acbobcat@protonmail.com>
2021-02-17 19:19:36 +0000

    groups and hardness values for all!

--- mods/bobcraft_minecraft_content/blocks/init.lua
+++ mods/bobcraft_minecraft_content/blocks/init.lua
@@ -6,24 +6,30 @@ minetest.register_node("bobcraft_blocks:grass_block", {
 	"dirt.png",
 	"grass_side.png"},
 	is_ground_content = true,
-	groups = {soil = 1, crumbly = 1},
-	sounds = bobcraft_sounds.node_sound_planty()
+	sounds = bobcraft_sounds.node_sound_planty(),
+
+	groups = {hand=1, shovel=1},
+	hardness = 0.6,
 })
 
 minetest.register_node("bobcraft_blocks:dirt", {
 	description = "Dirt Block",
 	tiles = {"dirt.png"},
 	is_ground_content = true,
-	groups = {soil = 1, crumbly = 2},
-	sounds = bobcraft_sounds.node_sound_earthy()
+	sounds = bobcraft_sounds.node_sound_earthy(),
+
+	groups = {hand=1, shovel=1},
+	hardness = 0.5,
 })
 
 minetest.register_node("bobcraft_blocks:stone", {
 	description = "Stone",
 	tiles = {"stone.png"},
 	is_ground_content = true,
-	groups = {cracky=1},
 	sounds = bobcraft_sounds.node_sound_stone(),
+
+	hardness = 1.5,
+	groups = {pickaxe=1},
 	drop = "bobcraft_blocks:cobblestone"
 })
 
@@ -32,7 +38,6 @@ minetest.register_node("bobcraft_blocks:rose", {
 	tiles = {"rose.png"},
 	wield_image = "rose.png",
 	is_ground_content = true,
-	groups = {oddly_breakable_by_hand=3},
 	sounds = bobcraft_sounds.node_sound_planty(),
 	drawtype = "plantlike",
 	walkable = false,
@@ -41,39 +46,50 @@ minetest.register_node("bobcraft_blocks:rose", {
 		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 7 / 16, 4 / 16}
 	},
 	paramtype = "light",
-	sunlight_propagates = true
+	sunlight_propagates = true,
+
+	groups = {hand=1},
+	hardness = 0,
 })
 
 minetest.register_node("bobcraft_blocks:cactus", {
 	description = "Cactus",
 	tiles = {"cactus_side.png"}, -- TODO: cactus model
-	groups = {oddly_breakable_by_hand=3},
-	sounds = bobcraft_sounds.node_sound_planty()
+	sounds = bobcraft_sounds.node_sound_planty(),
+
+	groups = {hand=1},
+	hardness = 0.4,
 })
 
 minetest.register_node("bobcraft_blocks:sand", {
 	description = "Sand",
 	tiles = {"sand.png"},
-	groups = {crumbly = 3, falling_node = 1, sand = 1},
 	sounds = bobcraft_sounds.node_sound_sand(),
+
+	groups = {hand=1, shovel=1, falling_node=1},
+	hardness = 0.5,
 })
 minetest.register_node("bobcraft_blocks:gravel", {
 	description = "Gravel",
 	tiles = {"gravel.png"},
-	groups = {crumbly = 3, falling_node = 1},
 	sounds = bobcraft_sounds.node_sound_gravel(),
+
+	groups = {hand=1, shovel=1, falling_node=1},
+	hardness = 0.6,
+	-- TODO: gravel/flint drops
 })
 minetest.register_node("bobcraft_blocks:sandstone", {
 	description = "Sand",
 	tiles = {"sandstone_top.png", "sandstone_bottom.png", "sandstone_side.png"},
-	groups = {crumbly = 3, sand = 1},
 	sounds = bobcraft_sounds.node_sound_sandstone(),
+
+	groups = {pickaxe=1},
+	hardness = 0.8,
 })
 
 minetest.register_node("bobcraft_blocks:snow_layer", {
 	description = "Snow",
 	tiles = {"snow.png"},
-	groups = {oddly_breakable_by_hand=2},
 	sounds = bobcraft_sounds.node_sound_snow(),
 	paramtype = "light",
 	buildable_to = true,
@@ -91,6 +107,9 @@ minetest.register_node("bobcraft_blocks:snow_layer", {
 			{-0.5, -0.5, -0.5, 0.5, -8 / 16, 0.5},
 		},
 	},
+
+	groups = {shovel=1},
+	hardness = 0.1,
 })
 minetest.register_node("bobcraft_blocks:snowy_grass_block", {
 	description = "Snow Covered Grass Block",
@@ -98,7 +117,6 @@ minetest.register_node("bobcraft_blocks:snowy_grass_block", {
 	"dirt.png",
 	"grass_side_snow.png"},
 	is_ground_content = true,
-	groups = {soil = 1, crumbly = 1},
 	sounds = bobcraft_sounds.node_sound_planty(),
 
 	on_construct = function(pos)
@@ -107,6 +125,9 @@ minetest.register_node("bobcraft_blocks:snowy_grass_block", {
 			minetest.set_node(pos, {name="bobcraft_blocks:snowy_grass_block"})
 		end
 	end,
+
+	groups = {shovel=1, hand=1},
+	hardness = 0.6,
 })
 
 -- liquids
@@ -139,7 +160,9 @@ minetest.register_node("bobcraft_blocks:water_source",{
 	liquid_alternative_source = "bobcraft_blocks:water_source",
 	liquid_viscosity = 1,
 	sounds = bobcraft_sounds.node_sound_water(),
-	groups = {not_in_creative_inventory=1}
+	groups = {not_in_creative_inventory=1},
+
+	hardness = 100 -- minecraft sets this, why?
 })
 minetest.register_node("bobcraft_blocks:water_flowing",{
 	description = "Water",
@@ -170,14 +193,16 @@ minetest.register_node("bobcraft_blocks:water_flowing",{
 	liquid_alternative_source = "bobcraft_blocks:water_source",
 	liquid_viscosity = 1,
 	sounds = bobcraft_sounds.node_sound_water(),
-	groups = {not_in_creative_inventory=1}
+	groups = {not_in_creative_inventory=1},
+
+	hardness = 100 -- minecraft sets this, why?
 })
 
 -- the token unbreakable block
 -- we have some code here to make it breakable in CREATIVE only
 local bedrock_groups
 if minetest.settings:get_bool("creative_mode") then
-	bedrock_groups = {oddly_breakable_by_hand=1}
+	bedrock_groups = {hand=1}
 else
 	bedrock_groups = {}
 end
@@ -186,7 +211,8 @@ minetest.register_node("bobcraft_blocks:bedrock", {
 	tiles = {"bedrock.png"},
 	is_ground_content = false, -- We *are* technically, but we also shouldn't be over-ridden
 	groups = bedrock_groups,
-	sounds = bobcraft_sounds.node_sound_stone()
+	sounds = bobcraft_sounds.node_sound_stone(),
+	hardness = -1
 })
 
 -- Other blocks
@@ -197,7 +223,6 @@ minetest.register_node("bobcraft_blocks:planks", {
 	is_ground_content = false,
 	sounds = bobcraft_sounds.node_sound_wood(),
 
-	--bobcraft values abound
 	groups = {axe=1},
 	hardness = 2
 })
@@ -206,36 +231,44 @@ minetest.register_node("bobcraft_blocks:glass", {
 	description = "Glass Block",
 	tiles = {"glass.png"},
 	is_ground_content = false,
-	groups = {cracky = 3, oddly_breakable_by_hand = 3},
 	sounds = bobcraft_sounds.node_sound_glass(),
 	drawtype = "glasslike",
 	paramtype = "light",
 	drop = {}, -- drop nothing
-	sunlight_propagates = true
+	sunlight_propagates = true,
+
+	hardness = 0.3,
+	groups = {pickaxe=1, hand=1, shovel=1, axe=1} -- TODO: I'm not sure what MC does here, but i mean, it's glass. Smashy smashy!
 })
 
 minetest.register_node("bobcraft_blocks:log", {
 	description = "Log",
 	tiles = {"log_top.png", "log_top.png", "log_side.png"},
 	is_ground_content = false,
-	groups = {choppy=1},
-	sounds = bobcraft_sounds.node_sound_wood()
+	sounds = bobcraft_sounds.node_sound_wood(),
+
+	hardness = 2,
+	groups = {axe=1, hand=1}
 })
 minetest.register_node("bobcraft_blocks:leaves", {
 	description = "Leaves",
 	drawtype = "allfaces_optional",
 	tiles = {"leaves.png"},
 	is_ground_content = false,
-	groups = {oddly_breakable_by_hand=1},
-	sounds = bobcraft_sounds.node_sound_planty()
+	sounds = bobcraft_sounds.node_sound_planty(),
+
+	hardness = 0.2,
+	groups = {hand=1} -- TODO: shears?
 })
 
 minetest.register_node("bobcraft_blocks:cobblestone", {
 	description = "Cobblestone",
 	tiles = {"cobblestone.png"},
 	is_ground_content = true,
-	groups = {cracky=1},
-	sounds = bobcraft_sounds.node_sound_stone()
+	sounds = bobcraft_sounds.node_sound_stone(),
+
+	hardness = 2, -- that's more than stone. interesting.
+	groups = {pickaxe=1}
 })
 
 local modpath = minetest.get_modpath("bobcraft_blocks")
