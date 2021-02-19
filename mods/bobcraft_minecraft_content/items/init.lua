775ef66287c0e4f7e5416ab01cd706565cdd0f38
ACBob <acbobcat@protonmail.com>
2021-02-19 02:18:26 +0000

    pork

--- mods/bobcraft_minecraft_content/items/init.lua
+++ mods/bobcraft_minecraft_content/items/init.lua
@@ -2,3 +2,14 @@ minetest.register_craftitem("bobcraft_items:sticks", {
 	description = "Stick",
 	inventory_image = "sticks.png"
 })
+
+minetest.register_craftitem("bobcraft_items:porkchop", {
+	description = "Raw Meat",
+	inventory_image = "porkchop.png",
+	on_use = minetest.item_eat(1.8)
+})
+minetest.register_craftitem("bobcraft_items:cooked_porkchop", {
+	description = "Cooked Meat",
+	inventory_image = "cooked_porkchop.png",
+	on_use = minetest.item_eat(12.8)
+})
