4666dc56a5376cfeb2d437ca2d81a90fb9e65fa6
ACBob <acbobcat@protonmail.com>
2021-02-21 06:11:48 +0000

    generate greendust

--- mods/bobcraft_minecraft_content/mapgen/ores.lua
+++ mods/bobcraft_minecraft_content/mapgen/ores.lua
@@ -36,6 +36,18 @@ minetest.register_ore({
 
 minetest.register_ore({
 	ore_type       = "blob",
+	ore            = "bobcraft_blocks:greendust_ore",
+	wherein        = "bobcraft_blocks:stone",
+	clust_scarcity = 16 * 8 * 16, -- Minecraft generates a set amount of ores per chunk. We can't do this. is 16*16*8 too common?
+	clust_num_ores = 7, -- Minecraft generates 16 blocks of coal ore.
+	clust_size     = 3, -- Not sure how I'd apply minecraft to this. It's the maximum number of blocks per side in the bounding box.
+	-- redstone generates from y=0, 16 in minecraft 1.2.5
+	y_max = worldgen.overworld_bottom+16,
+	y_min = worldgen.overworld_bottom,
+})
+
+minetest.register_ore({
+	ore_type       = "blob",
 	ore            = "bobcraft_blocks:diamond_ore",
 	wherein        = "bobcraft_blocks:stone",
 	clust_scarcity = 16 * 16 * 16, -- Minecraft generates a set amount of ores per chunk. We can't do this. is 16*16*16 too common?
