a3c176eca70d3bcdaa8811ccd4c5a4a02d14a8e0
ACBob <acbobcat@protonmail.com>
2021-02-22 20:58:12 +0000

    raise water level by one block huh, maybe there's something in that climate change thing...

--- mods/bobcraft_minecraft_content/mapgen/init.lua
+++ mods/bobcraft_minecraft_content/mapgen/init.lua
@@ -6,7 +6,7 @@ worldgen.overworld_top = 256
 worldgen.overworld_bottom = -64 -- Because moving the sea level fucked with v7's generation
 -- The water level seems to be 63 in 1.2.5
 -- We can assume the overworld_bottom is the relative 0 point
-worldgen.overworld_sea_level = worldgen.overworld_bottom+63
+worldgen.overworld_sea_level = worldgen.overworld_bottom+64
 
 minetest.register_alias("mapgen_water_source", "bobcraft_blocks:water_source")
 minetest.register_alias("mapgen_stone", "bobcraft_blocks:stone")
