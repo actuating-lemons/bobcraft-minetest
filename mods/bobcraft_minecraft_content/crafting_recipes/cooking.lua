f5f2aef256534515d44048ca3014816f1424ebed
ACBob <acbobcat@protonmail.com>
2021-02-22 17:57:10 +0000

    all of 1.2.5's recipes might as well, as minetest just works if the items don't exist

--- mods/bobcraft_minecraft_content/crafting_recipes/cooking.lua
+++ mods/bobcraft_minecraft_content/crafting_recipes/cooking.lua
@@ -1,9 +1,8 @@
 -- NOTE: Recipes should *pretty much* always have a cooktime of 10.
 -- Cooktime is measured in seconds, and minecraft uses 10 seconds to cook ALL items.
 
-----
--- Blocks
-----
+-- input sand
+-- get glass
 minetest.register_craft({
 	type = "cooking",
 	output = "bobcraft_blocks:glass",
@@ -11,6 +10,8 @@ minetest.register_craft({
 	cooktime = 10,
 })
 
+-- input iron ore
+-- get iron
 minetest.register_craft({
 	type = "cooking",
 	output = "bobcraft_items:iron_ingot",
@@ -18,9 +19,89 @@ minetest.register_craft({
 	cooktime = 10,
 })
 
+-- input gold ore
+-- get gold
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:gold_ingot",
+	recipe = "bobcraft_blocks:gold_ore",
+	cooktime = 10,
+})
+
+-- input diamond ore
+-- get diamond
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:diamond",
+	recipe = "bobcraft_blocks:diamond_ore",
+	cooktime = 10,
+})
+
+-- input coal ore
+-- get coal
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:coal_ore",
+	recipe = "bobcraft_blocks:coal",
+	cooktime = 10,
+})
+
+-- input greendust ore
+-- get exploded on
+-- TODO: we'll need to do this seperately, AND after we have explosions.
+
+-- input lapis ore
+-- get lapis
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:lapis",
+	recipe = "bobcraft_blocks:lapis_ore",
+	cooktime = 10,
+})
+
+-- input cobblestone
+-- get stone
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_blocks:stone",
+	recipe = "bobcraft_blocks:cobblestone",
+	cooktime = 10,
+})
+
+-- input clay lump
+-- get brick
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:clay",
+	recipe = "bobcraft_items:brick",
+	cooktime = 10,
+})
+
+-- input cactus
+-- get green dye
+minetest.register_craft({
+	type = "cooking",
+	output = dye.items.green,
+	recipe = "bobcraft_blocks:cactus",
+	cooktime = 10,
+})
+
+-- input log
+-- get coal (TODO: CHARCOAL)
+minetest.register_craft({
+	type = "cooking",
+	output = "bobcraft_items:coal",
+	recipe = "bobcraft_blocks:log",
+	cooktime = 10,
+})
+
+
 ----
 -- Food
 ----
+
+-- input raw porkchop
+-- get cooked porkchop
 minetest.register_craft({
 	type = "cooking",
 	output = "bobcraft_items:cooked_porkchop",
