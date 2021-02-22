ea0171e868f4c7616ddd709bba9724d8c3959b0a
ACBob <acbobcat@protonmail.com>
2021-02-22 17:05:02 +0000

    add door item texture, make doors floor dependent

--- mods/bobcraft_minecraft_content/blocks/doors.lua
+++ mods/bobcraft_minecraft_content/blocks/doors.lua
@@ -4,7 +4,7 @@
 doors.register_door("bobcraft_blocks:door", {
 	description = "Wooden Door",
 	inventory_image = "item_door.png",
-	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=2,door=1},
+	groups = {hand=1, axe=1, door=1, attached_node=1},
 	tiles_top = {"door_top.png"},
 	tiles_bottom = {"door_bottom.png"},
 	sounds = bobcraft_sounds.node_sound_wood(),
