9c4764013e8dd9b64966c3968bbf3af905da299b
ACBob <acbobcat@protonmail.com>
2021-02-24 20:05:11 +0000

    Stone slab/stair

--- mods/bobcraft_minecraft_content/slabnstairs/init.lua
+++ mods/bobcraft_minecraft_content/slabnstairs/init.lua
@@ -1,26 +1,46 @@
+local function register_stair(name, blockdef)
+	blockdef.paramtype = "light"
+	blockdef.paramtype2 = "facedir"
+	blockdef.drawtype = "nodebox"
+	blockdef.node_box = {
+		type = "fixed",
+		fixed = {
+			{ -0.5, -0.5, -0.5,
+			  0.5, 0, 0.5 },
+			{ -0.5, 0, 0,
+			  0.5, 0.5, 0.5 } 
+		}
+	}
+
+	minetest.register_node(name,blockdef)
+
+end
+
+local function register_slab(name, blockdef)
+	blockdef.paramtype = "light"
+	blockdef.paramtype2 = "facedir"
+	blockdef.drawtype = "nodebox"
+	blockdef.node_box = {
+		type = "fixed",
+		fixed = {
+			{ -0.5, -0.5, -0.5,
+			  0.5, 0, 0.5 },
+		}
+	}
+
+	minetest.register_node(name,blockdef)
+
+end
+
 local function register_extra_variants(namespace, blockname, blockdesc)
 	local namespaced_blockname = "bobcraft_blocks:" .. blockname
 	local blockdef = minetest.registered_nodes[namespaced_blockname]
 	if not blockdef then return end
 
 	-- STAIR
-	minetest.register_node(namespace .. blockname .. "_stair", {
+	register_stair(namespace .. blockname .. "_stair", {
 		description = blockdesc .. " Stair",
 		tiles = blockdef.tiles,
-
-		paramtype = "light",
-		paramtype2 = "facedir",
-
-		drawtype = "nodebox",
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{ -0.5, -0.5, -0.5,
-				  0.5, 0, 0.5 },
-				{ -0.5, 0, 0,
-				  0.5, 0.5, 0.5 } 
-			}
-		},
 		
 		groups = blockdef.groups,
 		sounds = blockdef.sounds,
@@ -30,22 +50,10 @@ local function register_extra_variants(namespace, blockname, blockdesc)
 	bobcraft_crafting.register_stair_craft(namespace .. blockname .. "_stair", namespaced_blockname)
 
 	-- SLAB
-	minetest.register_node(namespace .. blockname .. "_slab", {
+	register_slab(namespace .. blockname .. "_slab", {
 		description = blockdesc .. " Slab",
 		tiles = blockdef.tiles,
 
-		paramtype = "light",
-		paramtype2 = "facedir",
-
-		drawtype = "nodebox",
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{ -0.5, -0.5, -0.5,
-				  0.5, 0, 0.5 }
-			}
-		},
-		
 		groups = blockdef.groups,
 		sounds = blockdef.sounds,
 
@@ -58,3 +66,21 @@ end
 
 register_extra_variants("bobcraft_blocks_xtra:","cobblestone", "Cobblestone")
 register_extra_variants("bobcraft_blocks_xtra:","planks", "Wood")
+
+-- stone slab/stair
+local stonedef = minetest.registered_nodes["bobcraft_blocks:stone"]
+register_stair("bobcraft_blocks_xtra:stone_stair",{
+	tiles = {"stone_slab_side.png","stone_slab_top.png",
+	"stone_stair_side.png","stone_stair_side.png^[transformfx", "stone_slab_side.png"},
+	sounds = stonedef.sounds,
+	groups = stonedef.groups,
+	hardness = stonedef.hardness,
+})
+bobcraft_crafting.register_stair_craft("bobcraft_blocks_xtra:stone_stair", "bobcraft_blocks:stone")
+register_slab("bobcraft_blocks_xtra:stone_slab",{
+	tiles = {"stone_slab_top.png","stone_slab_top.png","stone_slab_side.png"},
+	sounds = stonedef.sounds,
+	groups = stonedef.groups,
+	hardness = stonedef.hardness,
+})
+bobcraft_crafting.register_slab_craft("bobcraft_blocks_xtra:stone_slab", "bobcraft_blocks:stone")
