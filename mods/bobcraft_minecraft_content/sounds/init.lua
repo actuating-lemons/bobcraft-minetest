a2aac4750d1ea1b26db753b0b934c271a2805f99
ACBob <acbobcat@protonmail.com>
2021-02-24 23:57:14 +0000

    Metal Sounds

--- mods/bobcraft_minecraft_content/sounds/init.lua
+++ mods/bobcraft_minecraft_content/sounds/init.lua
@@ -25,6 +25,14 @@ function bobcraft_sounds.node_sound_wood(table)
 	return bobcraft_sounds.node_sound_default(table)
 end
 
+function bobcraft_sounds.node_sound_metal(table)
+	table = table or {}
+	table.footstep = table.footstep or {name = "step_metal", gain = 0.2}
+	table.dug = table.dug or {name = "break_metal", gain = 0.3}
+	table.place = table.place or {name = "place_metal", gain = 1.0}
+	return bobcraft_sounds.node_sound_default(table)
+end
+
 
 function bobcraft_sounds.node_sound_earthy(table)
 	table = table or {}
