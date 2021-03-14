e15a61d90040947ef4ed7f26969f1d4eac217c03
ACBob <acbobcat@protonmail.com>
2021-03-14 00:42:49 +0000

    Switch to one door texture for wood

--- mods/bobcraft_minecraft_content/blocks/doors.lua
+++ mods/bobcraft_minecraft_content/blocks/doors.lua
@@ -5,8 +5,8 @@ doors.register_door("bobcraft_blocks:door", {
 	description = "Wooden Door",
 	inventory_image = "item_door.png",
 	groups = {hand=1, axe=1, door=1, attached_node=1},
-	tiles_top = {"door_top.png"},
-	tiles_bottom = {"door_bottom.png"},
+	tiles_top = {"door_wood.png^[verticalframe:2:0"},
+	tiles_bottom = {"door_wood.png^[verticalframe:2:1"},
 	sounds = bobcraft_sounds.node_sound_wood(),
 	sunlight = false,
 	
