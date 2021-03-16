9737329040f38823214f6be9d0d52f694ed46dff
ACBob <acbobcat@protonmail.com>
2021-03-16 21:06:50 +0000

    remove sheep for now

--- mods/bobcraft_minecraft_content/mobs_passive/init.lua
+++ mods/bobcraft_minecraft_content/mobs_passive/init.lua
@@ -16,59 +16,6 @@ local function brain(self)
 	end
 end
 
-minetest.register_entity("bobcraft_mobs:sheep", {
-	physical = true,
-	stepheight = 0.6,
-	collide_with_objects = true,
-	collisionbox = {-0.5,-0.5,-0.5, 0.5,0.5,0.5,},
-	visual = "mesh",
-	mesh = "sheep.b3d",
-	textures = {"sheep.png"},
-	static_save = true,
-	makes_footstep_sound = true,
-	
-	-- Required Mobkit
-	on_step = mobkit.stepfunc,
-	on_activate = mobkit.actfunc,
-	get_staticdata = mobkit.statfunc,
-
-	springiness = 0, -- TODO: Investigate
-	buoyancy = 0.75,
-	max_speed = 5,
-	view_range = 16,
-	lung_capacity = 10,
-	max_hp = 8,
-	timeout = 600, -- TODO: Investigate
-	sounds = {
-		idle = "pig_idle", -- PIG SOUND?
-		hurt = "mob_hit",
-	},
-
-	jump_height = 1,
-
-	brainfunc = brain,
-	
-	-- animations
-	animation = {
-		stand = {
-			range = {
-				x = 0,
-				y = 0,
-			},
-			speed = 15,
-			loop = true,
-		},
-		walk = {
-			range = {
-				x = 15,
-				y = 25,
-			},
-			speed = 15,
-			loop = true,
-		}
-	}
-})
-
 -- Pigge ;D
 minetest.register_entity("bobcraft_mobs:pig", {
 	physical = true,
