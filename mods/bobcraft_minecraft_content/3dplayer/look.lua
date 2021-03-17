2589dd51e3d527541e27bf9569e55eaa6673005d
ACBob <acbobcat@protonmail.com>
2021-03-17 16:00:09 +0000

    fix the head position

--- mods/bobcraft_minecraft_content/3dplayer/look.lua
+++ mods/bobcraft_minecraft_content/3dplayer/look.lua
@@ -1,6 +1,6 @@
 -- https://github.com/LoneWolfHT/headanim/
 minetest.register_globalstep(function(dtime)
 	for _, player in pairs(minetest.get_connected_players()) do
-		player:set_bone_position("Head", vector.new(0, 12, 0), vector.new(-math.deg(player:get_look_vertical()), 0, 0))
+		player:set_bone_position("Head", vector.new(0, 12.5, 0), vector.new(-math.deg(player:get_look_vertical()), 0, 0))
 	end
 end)
