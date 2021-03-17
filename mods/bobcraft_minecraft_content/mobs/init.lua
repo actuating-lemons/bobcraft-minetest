81febd858a35e17f9b5f5d561e95bc3dc03a575a
ACBob <acbobcat@protonmail.com>
2021-03-17 14:03:19 +0000

    Remove chat message spawner

--- mods/bobcraft_minecraft_content/mobs/init.lua
+++ mods/bobcraft_minecraft_content/mobs/init.lua
@@ -1,18 +1,5 @@
 local abr = minetest.get_mapgen_setting('active_block_range')
 
--- debugging
-minetest.register_on_chat_message(function(name, message)
-	local player = minetest.get_player_by_name(name)
-	local pos = player:get_pos()
-	if message == "baa" then
-		minetest.add_entity(pos, "bobcraft_mobs:sheep")
-	elseif message == "oink" then
-		minetest.add_entity(pos, "bobcraft_mobs:pig")
-	elseif message == "brains" then
-		minetest.add_entity(pos, "bobcraft_mobs:zombie")
-	end
-end)
-
 -- real spawning, copied from https://github.com/TheTermos/wildlife/blob/master/init.lua
 local spawn_rate = 0.1
 local spawn_reduction = 0.5
