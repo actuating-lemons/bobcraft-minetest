85d82d9a5cbcc1263e186ddafca13f16dc30b76e
ACBob <acbobcat@protonmail.com>
2021-03-17 15:44:23 +0000

    Animation improvements fixes Player Model Issues #19

--- mods/bobcraft_minecraft_content/3dplayer/init.lua
+++ mods/bobcraft_minecraft_content/3dplayer/init.lua
@@ -13,7 +13,8 @@ function player_model.get_model(playername)
 	return player_model.players_model[playername]
 end
 
-function player_model.set_player_animation(player, animation_name) 
+function player_model.set_player_animation(player, animation_name, speed)
+	local speed = speed or 1
 	local playername = player:get_player_name()
 	local modelname = player_model.get_model(playername)
 	local model = player_model.models[modelname]
@@ -27,7 +28,7 @@ function player_model.set_player_animation(player, animation_name)
 
 	local anim = model.animations[animation_name]
 	player_model.players_animation[playername] = animation_name
-	player:set_animation(anim, model.animation_speed, 0)
+	player:set_animation(anim, model.animation_speed*speed, 0)
 end
 
 function player_model.set_model(player, modelname)
@@ -57,37 +58,37 @@ minetest.register_globalstep(function()
 		local model = model_name and player_model.models[model_name]
 		if model then
 			local controls = player:get_player_control()
-			local speed = 15
+			local speed = controls.sneak and 0.5 or 1
 
 			if not player_model.dont_animate[player_name] then
 				if controls.up or controls.down or controls.right or controls.left then
 					-- Moving
 					if controls.sneak then
-						player_model.set_player_animation(player, "sneakwalk")
+						player_model.set_player_animation(player, "sneakwalk", speed)
 					else
-						player_model.set_player_animation(player, "walk")
+						player_model.set_player_animation(player, "walk", speed)
 					end
 				else
 					-- Standing Still
 					if controls.sneak then
 						-- Sneaking
 						if controls.dig or controls.place then 
-							player_model.set_player_animation(player, "sneakpunch")
+							player_model.set_player_animation(player, "sneakpunch", speed)
 						else
-							player_model.set_player_animation(player, "sneak")
+							player_model.set_player_animation(player, "sneak", speed)
 						end
 					else
 						-- Not Sneaking
 						if controls.dig or controls.place then 
-							player_model.set_player_animation(player, "punch")
+							player_model.set_player_animation(player, "punch", speed)
 						else
-							player_model.set_player_animation(player, "stand")
+							player_model.set_player_animation(player, "stand", speed)
 						end
 					end
 				end
 
 				if player:get_hp() == 0 then
-					player_model.set_player_animation(player, "lay")
+					player_model.set_player_animation(player, "lay", speed)
 				end
 			end
 		end
@@ -103,12 +104,21 @@ player_model.register_model("player.b3d", {
 	animations = {
 		stand = {
 			x = 0,
-			y = 10
+			y = 30
 		},
 		walk = {
-			x = 15,
-			y = 25
+			x = 40,
+			y = 50
 		},
+
+		sneak = {
+			x = 55,
+			y = 75,
+		},
+		sneakwalk = {
+			x = 80,
+			y = 90,
+		}
 	}
 })
 
