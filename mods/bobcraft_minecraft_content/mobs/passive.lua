9d699e345fc24c2ac84d7954010d8823abe920de
ACBob <acbobcat@protonmail.com>
2021-03-17 22:25:27 +0000

    begin work on fire fish

--- mods/bobcraft_minecraft_content/mobs/passive.lua
+++ mods/bobcraft_minecraft_content/mobs/passive.lua
@@ -85,3 +85,73 @@ minetest.register_entity("bobcraft_mobs:pig", {
 	}
 })
 mobkit.register_spawn_egg("bobcraft_mobs:pig", "#eb9592", "#623637")
+
+-- Pigge ;D
+minetest.register_entity("bobcraft_mobs:firefish", {
+	physical = true,
+	stepheight = 0.6,
+	collide_with_objects = true,
+	collisionbox = {-0.15, 0.0, -0.15, 0.15, 0.3, 0.15,},
+	visual = "mesh",
+	mesh = "pig.b3d",
+	textures = {"pig.png"},
+	static_save = true,
+	makes_footstep_sound = true,
+	
+	-- Required Mobkit
+	on_step = mobkit.stepfunc,
+	on_activate = mobkit.actfunc,
+	get_staticdata = mobkit.statfunc,
+
+	springiness = 0, -- TODO: Investigate
+	buoyancy = 0.75,
+	max_speed = 5,
+	view_range = 16,
+	lung_capacity = 10,
+	max_hp = 10,
+	timeout = 600, -- TODO: Investigate
+	sounds = {
+		idle = "pig_idle",
+		hurt = "mob_hit",
+		die = "pig_death"
+	},
+
+	jump_height = 1,
+
+	brainfunc = water_brain,
+
+	on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir)
+		if mobkit.is_alive(self) then
+			local hvel = vector.multiply(vector.normalize({x=dir.x,y=0,z=dir.z}),4)
+			self.object:set_velocity({x=hvel.x,y=2,z=hvel.z})
+			
+			mobkit.hurt(self,tool_capabilities.damage_groups.fleshy or 1)
+			mobkit.make_sound(self,'hurt')
+
+			-- abandon ship, run away!
+			mobkit.clear_queue_high(self)
+			mobkit.hq_runfrom(self, 10, puncher)
+		end
+	end,
+	
+	-- animations
+	animation = {
+		stand = {
+			range = {
+				x = 0,
+				y = 0,
+			},
+			speed = 15,
+			loop = true,
+		},
+		walk = {
+			range = {
+				x = 15,
+				y = 25,
+			},
+			speed = 15,
+			loop = true,
+		}
+	}
+})
+mobkit.register_spawn_egg("bobcraft_mobs:pig", "#eb9592", "#623637")
