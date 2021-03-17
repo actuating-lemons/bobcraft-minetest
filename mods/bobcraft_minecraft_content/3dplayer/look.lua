5911d1db087c56822ab0666bd7c770affad55a3a
ACBob <acbobcat@protonmail.com>
2021-03-17 16:30:19 +0000

    fix bone positions

--- mods/bobcraft_minecraft_content/3dplayer/look.lua
+++ mods/bobcraft_minecraft_content/3dplayer/look.lua
@@ -2,6 +2,6 @@
 minetest.register_globalstep(function(dtime)
 	for _, player in pairs(minetest.get_connected_players()) do
 		local player_look_deg = math.deg(player:get_look_vertical())
-		player:set_bone_position("Head", vector.new(0, 12.5, 0), vector.new(-player_look_deg, 0, 0))
+		player:set_bone_position("Head", vector.new(0, 12, 0), vector.new(-player_look_deg, 0, 0))
 	end
 end)
