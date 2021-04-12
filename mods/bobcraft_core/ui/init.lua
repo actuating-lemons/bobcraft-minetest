3103846338a9078e0e5e18a49781dc5193593671
ACBob <acbobcat@protonmail.com>
2021-04-12 18:14:53 +0100

    add subtle formspec hover

--- mods/bobcraft_core/ui/init.lua
+++ mods/bobcraft_core/ui/init.lua
@@ -2,7 +2,7 @@ minetest.register_on_joinplayer(function(player)
 	player:set_formspec_prepend(
 	-- background
 	"background9[1,1;1,1;formspec_bg.png;true;7]" ..
-	"listcolors[#0000;#0000;#000f;#000f;#ffff]" ..
+	"listcolors[#0000;#fff4;#000f;#000f;#ffff]" ..
 	-- Buttons
 	"style_type[button;textcolor=#000000;border=false;bgimg=formspec_button_bg.png;bgimg_pressed=formspec_button_bg_pressed.png;bgimg_hovered=formspec_button_bg_hover.png;bgimg_middle=5,5]" ..
 	-- Text
